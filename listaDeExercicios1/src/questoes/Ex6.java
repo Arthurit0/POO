62d2c36f015e444126684b9ac969b1f6630f0012
Arthur José <arthur.jose27@gmail.com>
2021-10-07 22:18:05 -0300

    Mais mudanças
    Só fazendo anotações para mim mesmo do futuro caso esqueça como o código funciona. Falta ordenar o array de chars em ordem alfabética

--- listaDeExercicios1/src/questoes/Ex6.java
+++ listaDeExercicios1/src/questoes/Ex6.java
@@ -12,43 +12,46 @@ public class Ex6 {
         System.out.println("Digite a String nº2: ");
         String S2 = scanner.next();
         System.out.println();
+        scanner.close();
 
         int L1 = S1.length();
         int L2 = S2.length();
 
-        String F = "";
-
         char V[] = new char[L1+L2];
         int indexV = 0, contV = 0;
 
-        //Coletando letras iguais em ambas as Strings
+        //Comparando cada letra da primeira string com a da segunda. 
         for (int i=0; i<L1; i++){
             for (int j=0; j<L2; j++){
                 if (S1.toLowerCase().charAt(i) == S2.toLowerCase().charAt(j)){
-                    F = F + S1.toUpperCase().charAt(i);
 
+                    //if guarda a primeira letra encontrada em ambas as strings na primeira posição do vetor de array
                     if (indexV == 0){
                         V[indexV] = S1.toUpperCase().charAt(i);
                         indexV++;
                     }else{
+                        //Percorrendo vetor do início até o contador indexV
                         for (int k=0; k<indexV;k++){
+                            //Comparando se a letra igual encontrada pelos dois fors já está no array V, se estiver,
+                            //o contador aumenta para que não entre no próximo if
                             if (S1.toUpperCase().charAt(i) == V[k]){
                                 contV++;
                             }
                         }
+                        //if para filtrar se a letra encontrada se repetiu no array V, se não for o caso,
+                        //a letra será guardada na próxima posição de V
                         if (contV == 0){
                             V[indexV] = S1.toUpperCase().charAt(i);
                             indexV++;
                         }
                         contV = 0;
                     }
-                    
-
                 }
             }
         }
-
-        System.out.println(F);
         System.out.println(V);
+
+        //Ordenando em ordem Alfabética
+
     }
 }
