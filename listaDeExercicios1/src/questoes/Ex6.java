c44f2d0d215db2325796b4ad59f27e47027b80e4
Arthur José <arthur.jose27@gmail.com>
2021-10-08 09:08:18 -0300

    Revert "EX6 aprimorado"
    This reverts commit a2c4bfc36be400a4fe0a2bc12a72a61db044168b.

--- listaDeExercicios1/src/questoes/Ex6.java
+++ listaDeExercicios1/src/questoes/Ex6.java
@@ -17,8 +17,7 @@ public class Ex6 {
         int L1 = S1.length();
         int L2 = S2.length();
 
-        String V = new String();
-        V = "";
+        char V[] = new char[L1+L2];
         int indexV = 0, contV = 0;
 
         //Comparando cada letra da primeira string com a da segunda. 
@@ -26,23 +25,23 @@ public class Ex6 {
             for (int j=0; j<L2; j++){
                 if (S1.toLowerCase().charAt(i) == S2.toLowerCase().charAt(j)){
 
-                    //if guarda a primeira letra encontrada em ambas as strings na primeira posição da string
+                    //if guarda a primeira letra encontrada em ambas as strings na primeira posição do vetor de array
                     if (indexV == 0){
-                        V = V + S1.toUpperCase().charAt(i);
+                        V[indexV] = S1.toUpperCase().charAt(i);
                         indexV++;
                     }else{
                         //Percorrendo vetor do início até o contador indexV
                         for (int k=0; k<indexV;k++){
-                            //Comparando se a letra igual encontrada pelos dois fors já está na string V, se estiver,
+                            //Comparando se a letra igual encontrada pelos dois fors já está no array V, se estiver,
                             //o contador aumenta para que não entre no próximo if
-                            if (S1.toUpperCase().charAt(i) == V.toUpperCase().charAt(k)){
+                            if (S1.toUpperCase().charAt(i) == V[k]){
                                 contV++;
                             }
                         }
-                        //if para filtrar se a letra encontrada se repetiu na string, se não for o caso,
+                        //if para filtrar se a letra encontrada se repetiu no array V, se não for o caso,
                         //a letra será guardada na próxima posição de V
                         if (contV == 0){
-                            V = V + S1.toUpperCase().charAt(i);
+                            V[indexV] = S1.toUpperCase().charAt(i);
                             indexV++;
                         }
                         contV = 0;
@@ -53,6 +52,6 @@ public class Ex6 {
         System.out.println(V);
 
         //Ordenando em ordem Alfabética
-        
+
     }
 }
