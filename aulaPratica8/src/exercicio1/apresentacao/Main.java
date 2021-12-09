62f1edd467adbb7343d51e610999d255ace75694
Arthur0 <57883830+Arthurit0@users.noreply.github.com>
2021-12-09 10:27:00 -0300

    Aula sobre Banco de Dados

---
+++ aulaPratica8/src/exercicio1/apresentacao/Main.java
@@ -0,0 +1,8 @@
+package exercicio1.apresentacao;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ aulaPratica8/src/exercicio2/dados/Contato.java
@@ -0,0 +1,40 @@
+package exercicio2.dados;
+
+public class Contato {
+    private String nome;
+    private int telefone;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean equals(Object o){
+        Contato c = (Contato) o;
+        if(this.telefone == c.getTelefone()){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public String toString() {
+        String str = "";
+
+        str += "- "+this.nome+": "+this.telefone;
+
+        return str;
+    }
+
+}
